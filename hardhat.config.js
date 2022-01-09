fe62c2805e2ec8f35dd866d684cd0d9fd5dcd6ba
yasirghouri <yasir.ag7@gmail.com>
2022-01-09 16:29:25 +0500

    Project initialization and configuration

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    },
+  },
+  solidity: "0.8.4",
+};
