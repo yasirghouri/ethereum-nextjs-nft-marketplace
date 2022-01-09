e7985c687b45ed321e1f3d8a961678e70c996f35
yasirghouri <yasir.ag7@gmail.com>
2022-01-09 17:28:16 +0500

    test case for our contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.METAMASK_PRIVATE_KEY}`],
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      // accounts: [`${process.env.METAMASK_PRIVATE_KEY}`],
+      accounts: [privateKey],
     },
-    mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.METAMASK_PRIVATE_KEY}`],
-    },
+    // mumbai: {
+    //   url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    //   accounts: [`${process.env.METAMASK_PRIVATE_KEY}`],
+    // },
+    // mainnet: {
+    //   url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    //   accounts: [`${process.env.METAMASK_PRIVATE_KEY}`],
+    // },
   },
   solidity: "0.8.4",
 };
